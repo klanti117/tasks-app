61ca29d4567b13df9882210d5bf238ee24701f37
klanti117 <klantyislam@yahoo.com>
2025-04-20 22:26:39 -0400

    added conclusion

--- data.js
+++ data.js
@@ -1,1 +1,9 @@
-//TODO: Add an in-memory array of tasks
+//TODO: As part of the lab, you will define and populate an array of task objects. Each task object should include the following fields:
+
+/*
+    {
+        // Unique identifier (number)
+        // Short description of the task (string)
+        // Whether the task is important (boolean)
+    }
+*/
